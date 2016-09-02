ce0690f46796e72bca327971dbaadb34a8a5d788
Sainath Adapa <adapasainath@gmail.com>
2016-09-02 22:21:11 +0530

    Fixing more stuff

--- R/search_for_datasets.R
+++ R/search_for_datasets.R
@@ -118,16 +118,20 @@ get_datasets_from_a_catalog <- function(catalog_link, limit_dataset_pages = 5L, 
   
   links_tried <- character(0)
   
-  datasets <- data.frame(name        = character(0),
-                         granularity = character(0),
-                         file_size   = character(0),
-                         downloads   = numeric(0),
-                         res_id      = character(0),
-                         ods         = character(0),
-                         xls         = character(0),
-                         json        = character(0),
-                         xml         = character(0),
-                         jsonp       = character(0))
+  datasets <- data.frame(
+    name             = character(0),
+    granularity      = character(0),
+    file_size        = character(0),
+    downloads        = numeric(0),
+    res_id           = character(0),
+    csv              = character(0),
+    ods              = character(0),
+    xls              = character(0),
+    json             = character(0),
+    xml              = character(0),
+    jsonp            = character(0),
+    stringsAsFactors = FALSE
+  )
   
   this_link <- catalog_link
   
@@ -145,7 +149,7 @@ get_datasets_from_a_catalog <- function(catalog_link, limit_dataset_pages = 5L, 
     
     datasets <- rbind(datasets, this_datasets)
     
-    message('Found ', nrow(datasets), ' datasets till now')
+    message('Found ', nrow(datasets), ' datasets till now in this catalog')
     
     next_pages <- this_catalog_result %>% html_nodes(css = '.pager-item a') %>% html_attr('href')
     next_pages <- vapply(X = next_pages, FUN = mk_link, FUN.VALUE = 'temp', USE.NAMES = FALSE)
@@ -161,12 +165,13 @@ get_datasets_from_a_catalog <- function(catalog_link, limit_dataset_pages = 5L, 
 }
 
 #' @title Search for data sets
-#' @description This function scrapes the data.gov.in search results and returns all the information available for the datasets. As this function doesn't use API and just parses the web pages, there needs to delay between successive requests, and there should be limits to the number of pages that the function downloads from the web. For a particular search input, there may be multiple pages of search results. Each result page contains a list of catalogs. And each catalog contains multiple pages, with each page containing a list of data sets. There are default limits at each one of these stages. Make them 'Inf' if you need to get all the results or if you don't expect a large number of results.
+#' @description This function scrapes the data.gov.in search results and returns most of the information available for the datasets. As this function doesn't use API and just parses the web pages, there needs to delay between successive requests, and there should be limits to the number of pages that the function downloads from the web. For a particular search input, there may be multiple pages of search results. Each result page contains a list of catalogs. And each catalog contains multiple pages, with each page containing a list of data sets. There are default limits at each one of these stages. Make them 'Inf' if you need to get all the results or if you don't expect a large number of results. Please refer to vignette for a detailed overview.
 #' @param search_terms Either one string with multiple words separated by space, or a character vector with all the search terms
-#' @param limit_search_pages Number of pages of search results to request. Default is 5. Make it Inf to get all.
-#' @param return_catalog_list Default is FALSE. If TRUE, the function return will not look for data sets, and will only return the list of catalogs found.
-#' @param limit_catalogs Number of catalogs that the function should parse to get the data sets. Default is 5. Make it Inf to get all.
-#' @param limit_catalog_pages Number of pages to parse through, for each catalog. Default is 5. Make it Inf to get all.
+#' @param limit_catalog_pages Number of pages of search results to request. Default is 5. Set to Inf to get all.
+#' @param limit_catalogs Number of catalogs that the function should parse to get the data sets. Default is 5. Set to Inf to get all.
+#' @param return_catalog_list Default is FALSE. If TRUE, the function will not look for data sets, and will only return the list of catalogs found.
+#' @param limit_dataset_pages Limit the number of pages that should be requested and parsed, to acquire the datasets. Default is 5. Set to Inf to request all.
+#' @param limit_datasets Request more pages until the number of datasets obtained reaches this limit. Default is 10. Set to Inf to request all.
 #' @importFrom magrittr %>%
 #' @importFrom xml2 read_html
 #' @importFrom rvest html_nodes html_text html_attr
@@ -178,19 +183,20 @@ get_datasets_from_a_catalog <- function(catalog_link, limit_dataset_pages = 5L, 
 #' 
 #' # Advanced Use, specifying additional parameters
 #' search_for_datasets(search_terms = c('state', 'gdp'),
-#'                     limit_search_pages = 1,
+#'                     limit_catalog_pages = 1,
 #'                     limit_catalogs = 3,
-#'                     limit_catalog_pages = 2)
+#'                     limit_dataset_pages = 2)
 #' search_for_datasets(search_terms = c('state', 'gdp'),
-#'                     limit_search_pages = 2,
+#'                     limit_catalog_pages = 2,
 #'                     return_catalog_list = TRUE)
 #' }
 #' @seealso get_datasets_from_a_catalog
 search_for_datasets <- function(search_terms,
-                                limit_search_pages = 5L,
-                                limit_catalogs = 5L,
+                                limit_catalog_pages = 5L,
+                                limit_catalogs      = 10L,
                                 return_catalog_list = FALSE,
-                                limit_catalog_pages = 5L) {
+                                limit_dataset_pages = 5L,
+                                limit_datasets      = 10L) {
   
   #TODO: Escaping of search terms
   search_terms_collapsed <- search_terms %>%
@@ -203,16 +209,27 @@ search_for_datasets <- function(search_terms,
                           '&sort_order=DESC',
                           '&items_per_page=9')
   
-  search_result <- get_link(mk_link(relative_link))
   
-  links_tried <- relative_link
+  links_tried <- character(0)
   
-  catalogs <- extract_catalogs_from_search_result(search_result)
+  catalogs <- data.frame(
+    name = character(0),
+    link = character(0),
+    stringsAsFactors = FALSE
+  )
   
-  message('Found ', nrow(catalogs), ' catalogs till now')
+  this_link <- relative_link
   
-  while ( (length(links_tried) < limit_search_pages) && 
-          (nrow(catalogs) < limit_catalogs) ) {
+  while ( (length(links_tried) < limit_catalog_pages) && 
+          (nrow(catalogs)      < limit_catalogs) ) {
+    
+    search_result <- get_link(mk_link(this_link))
+    links_tried <- c(links_tried, this_link)
+    
+    catalogs <- rbind(catalogs,
+                      extract_catalogs_from_search_result(search_result))
+    
+    message('Found ', nrow(catalogs), ' catalogs till now')
     
     next_pages_links <- search_result %>%
       html_nodes(css = '.pager-item a') %>%
@@ -222,16 +239,7 @@ search_for_datasets <- function(search_terms,
     
     if (length(next_pages_links) < 1) break
     
-    next_page_rel_link <- next_pages_links[1]
-    
-    search_result <- get_link(mk_link(next_page_rel_link))
-    
-    links_tried <- c(links_tried, next_page_rel_link)
-    
-    catalogs <- rbind(catalogs,
-                      extract_catalogs_from_search_result(search_result))
-    
-    message('Found ', nrow(catalogs), ' catalogs till now')
+    this_link <- next_pages_links[1]
   }
   
   if (return_catalog_list) {
@@ -242,29 +250,34 @@ search_for_datasets <- function(search_terms,
     return(catalogs) 
   }
   
-  ans <- lapply(X = catalogs_from_all_search_pages$link,
-                FUN = . %>%
-                  mk_link %>%
-                  get_datasets_from_a_catalog(.,limit_pages = limit_catalog_pages)
-  ) %>% 
-    do.call(what = rbind, args = .)
+  datasets <- data.frame(
+    name             = character(0),
+    granularity      = character(0),
+    file_size        = character(0),
+    downloads        = numeric(0),
+    res_id           = character(0),
+    csv              = character(0),
+    ods              = character(0),
+    xls              = character(0),
+    json             = character(0),
+    xml              = character(0),
+    jsonp            = character(0),
+    stringsAsFactors = FALSE
+  )
   
-  if (is.null(ans) || (nrow(ans) == 0)) {
-    return(data.frame(
-      name             = character(0),
-      granularity      = character(0),
-      file_size        = character(0),
-      downloads        = numeric(0),
-      res_id           = character(0),
-      csv              = character(0),
-      ods              = character(0),
-      xls              = character(0),
-      json             = character(0),
-      xml              = character(0),
-      jsonp            = character(0),
-      stringsAsFactors = FALSE
-    ))
+  for (one_catalog in catalogs$link) {
+    this_datasets <- get_datasets_from_a_catalog(
+      catalog_link = mk_link(one_catalog),
+      limit_dataset_pages = limit_dataset_pages,
+      limit_datasets = limit_datasets
+    )
+    
+    datasets <- rbind(datasets, this_datasets)
+    
+    message('Found a total of ', nrow(datasets), ' datasets till now')
+    
+    if (nrow(datasets) > limit_datasets) break
   }
   
-  ans
+  datasets
 }
